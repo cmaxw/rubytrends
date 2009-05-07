3146111a7df430a41292ad2c07d052fe25516e24
Jared Dobson <jaredd@pmamediagroup.com>
2009-05-07 11:40:04 -0700

    fixed

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,3 @@
 class Asset < ActiveRecord::Base
   belongs_to :task
-  def self.all_to_select
-     Asset.all.collect { |asset| [asset.name, asset.id] }
-  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :milestone
   has_many :assets
-  def self.all_to_select
-     Task.all.collect { |task| [task.name, task.id] }
-  end
 end
