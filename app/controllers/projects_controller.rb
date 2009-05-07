6d431e0d4ddb1dbacf3c4942a3ff4f2907abb567
Jared Dobson <jaredd@pmamediagroup.com>
2009-05-06 21:31:06 -0700

    test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.find(:all, :include => :milestone)
+    @projects = Project.find(:all, :include => :milestones)
 
     respond_to do |format|
       format.html # index.html.erb
