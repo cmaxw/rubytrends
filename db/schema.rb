70edda34c3bdd0605010403bfe6dca8e0f76c8ee
Chuck Wood <chuck@pmamediagroup.com>
2009-05-07 12:08:41 -0700

    removed extra migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 
 ActiveRecord::Schema.define(:version => 20090507031850) do
 
-  create_table "items", :force => true do |t|
+  create_table "assets", :force => true do |t|
     t.string   "name"
     t.integer  "task_id"
     t.datetime "created_at"
